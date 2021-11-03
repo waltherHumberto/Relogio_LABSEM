9dabafb29de046c47091a0172416c00f8fc03086
waltherHumberto <70277063+waltherHumberto@users.noreply.github.com>
2021-11-03 20:07:47 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,13 +1,24 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <LiquidCrystal.h>
+#include <string.h>
 
 LiquidCrystal lcd(8, 9, 10, 11, 12, 13);
-
 #define TIMER1_MODULE 15625
 #define FALSE 0
 #define TRUE 1
 #define _ltrans(x, xi, xm, yi, ym) (long)((long)(x - xi) * (long)(ym - yi)) / (long)(xm - xi) + yi
+#define NORMAL 0
+#define CRON_START 1
+#define CRON_PAUSE 2
+#define CRON_STOP 3
+#define ALARM_SEG 4
+#define ALARM_MIN 5
+#define ALARM_HRA 6
+#define REL_SEG 7
+#define REL_MIN 8
+#define REL_HRA 9
+#define ALARME_ATIV 10
 
 typedef struct
 {
@@ -21,7 +32,8 @@ typedef enum
 {
     HORA_ATUAL,
     CRONOMETRO,
-    ALARME_REL
+    ALARME_REL,
+    ACERTA_REL
 
 } relogio_state;
 
@@ -33,17 +45,28 @@ void conta_cronometro(Rel *r);
 
 relogio_state Estado_Rel = HORA_ATUAL;
 char Seleciona = FALSE;
+int counter = 0;
 Rel *pts;
 
-char *mensagem[4] = {"Hora Certa  ",
-                     "Setar Alarme",
-                     "Cronometro  ",
-                     "Alterar HR  "};
+char *mensagem[20] = {
+    "   Hora Atual   ",
+    "Crono - Iniciar ",
+    " Crono - Pausar ",
+    " Crono - Parar  ",
+    "Set Alarme - Seg",
+    "Set Alarme - Min",
+    "Set Alarme - Hra",
+    " Set Rel - Seg  ",
+    " Set Rel - Min  ",
+    " Set Rel - Hra  ",
+    "   ALARME !!!!  "};
+
+int index = 0;
 
 int main(void)
 {
     Rel Relogio = {0, 0, 0, 0};
-    Rel Alarme = {0, 0, 0, 0};
+    Rel Alarme = {9, 9, 9, 9};
     pts = &Relogio;
     init_dsp(2, 16);
     putmessage(0, 3, mensagem[1]);
@@ -55,28 +78,27 @@ int main(void)
     EICRA = _BV(ISC01) | _BV(ISC00);
     EIMSK = _BV(INT0) | _BV(INT1);
     sei();
-    int index = 0;
+
     for (;;)
     {
-        putmessage(0, 3, mensagem[index]);
-
+        putmessage(0, 0, mensagem[index]);
         switch (Estado_Rel)
         {
         case HORA_ATUAL:
             print_hora_atual(&Relogio, &Alarme);
-            index = 0;
             break;
 
         case ALARME_REL:
             configura_alarme(&Alarme);
-            index = 1;
             break;
 
         case CRONOMETRO:
             conta_cronometro(&Relogio);
-            index = 2;
             break;
 
+        case ACERTA_REL:
+            AcertaRelogio(&Relogio);
+            break;
         default:
             break;
         }
@@ -96,18 +118,22 @@ void AcertaRelogio(Rel *r)
     switch (State)
     {
     case 0:
+        index = REL_HRA;
         r->hora = _ltrans(Le_AD(0), 0, 1023, 0, 23);
         putnumber_i(1, 4, r->hora, 2);
         break;
     case 1:
+        index = REL_MIN;
         r->minuto = _ltrans(Le_AD(0), 0, 1023, 0, 59);
         putnumber_i(1, 7, r->minuto, 2);
         break;
     case 2:
+        index = REL_SEG;
         r->segundo = _ltrans(Le_AD(0), 0, 1023, 0, 59);
         putnumber_i(1, 10, r->segundo, 2);
         break;
     case 3:
+        index = NORMAL;
         Estado_Rel = HORA_ATUAL;
         break;
     }
@@ -126,14 +152,17 @@ void configura_alarme(Rel *a)
     switch (State)
     {
     case 0:
+        index = ALARM_HRA;
         a->hora = _ltrans(Le_AD(0), 0, 1023, 0, 23);
         putnumber_i(1, 4, a->hora, 2);
         break;
     case 1:
+        index = ALARM_MIN;
         a->minuto = _ltrans(Le_AD(0), 0, 1023, 0, 59);
         putnumber_i(1, 7, a->minuto, 2);
         break;
     case 2:
+        index = ALARM_SEG;
         a->segundo = _ltrans(Le_AD(0), 0, 1023, 0, 59);
         putnumber_i(1, 10, a->segundo, 2);
         break;
@@ -146,16 +175,20 @@ void configura_alarme(Rel *a)
 void print_hora_atual(Rel *r, Rel *A)
 {
 
+    if (A->hora == r->hora && A->minuto == r->minuto)
+    {
+        index = ALARME_ATIV;
+    }
+    else
+    {
+        index = NORMAL;
+    }
     if (r->segundoAnt != r->segundo)
     {
         r->segundoAnt = r->segundo;
         putnumber_i(1, 4, r->hora, 2);
         putnumber_i(1, 7, r->minuto, 2);
         putnumber_i(1, 10, r->segundo, 2);
-    }
-    if (r == A)
-    {
-        putmessage(1, 4, "alarme");
     }
 }
 
@@ -167,27 +200,59 @@ void conta_cronometro(Rel *r)
     {
         Seleciona = FALSE;
         State++;
-        State %= 4;
+        State %= 3;
     }
-
-    if (r->segundoAnt != r->segundo)
+    switch (State)
     {
-        r->segundoAnt = r->segundo;
-        putnumber_i(1, 4, r->hora, 2);
-        putnumber_i(1, 7, r->minuto, 2);
-        putnumber_i(1, 10, r->segundo, 2);
+    case 0:
+        index = CRON_START;
+        break;
+    case 1:
+        index = CRON_PAUSE;
+        if (r->segundoAnt != r->segundo)
+        {
+            r->segundoAnt = r->segundo;
+            putnumber_i(1, 4, r->hora, 2);
+            putnumber_i(1, 7, r->minuto, 2);
+            putnumber_i(1, 10, r->segundo, 2);
+        }
+    case 3:
+        index = NORMAL;
+        Estado_Rel = HORA_ATUAL;
+        break;
+
+    default:
+        break;
     }
 }
 
-ISR(INT0_vect) // Pára o relógio  e Configura o Alarme
+ISR(INT0_vect) // Seleciona o Modo
 {
-    Estado_Rel = ALARME_REL;
-    Seleciona = TRUE;
+
+    if (Estado_Rel == HORA_ATUAL)
+    {
+        index = CRON_START;
+        Estado_Rel = CRONOMETRO;
+    }
+    else if (Estado_Rel == CRONOMETRO)
+    {
+        index = ALARM_HRA;
+        Estado_Rel = ALARME_REL;
+    }
+    else if (Estado_Rel == ALARME_REL)
+    {
+        index = REL_HRA;
+        Estado_Rel = ACERTA_REL;
+    }
+    else if (Estado_Rel == ACERTA_REL)
+    {
+        index = NORMAL;
+        Estado_Rel = HORA_ATUAL;
+    }
 }
 
-ISR(INT1_vect) // Pára o relógio e prepara  o Cronometro
+ISR(INT1_vect) //Seleciona o modo
 {
-    Estado_Rel = CRONOMETRO;
     Seleciona = TRUE;
 }
 
